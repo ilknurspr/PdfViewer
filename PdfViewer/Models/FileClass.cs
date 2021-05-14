bfd985d91ef4bcd468a1f02bf814d99fa7012310
ilknurspr <61519644+ilknurspr@users.noreply.github.com>
2021-05-14 11:42:57 +0300

    Pdf viewer

---
+++ PdfViewer/Models/FileClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfViewer.Models
+{
+    public class FileClass
+    {
+        public int FileId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Path { get; set; } = "";
+        public List<FileClass> Files { get; set; } = new List<FileClass>();
+    }
+}
